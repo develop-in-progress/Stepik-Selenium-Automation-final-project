a58e14d001b66a82d5ce7d24bbe585479f0a4c41
develop-in-progress <hellfree22@gmail.com>
2020-11-09 19:21:17 +0200

    added Travi CIs

--- conftest.py
+++ conftest.py
@@ -14,12 +14,19 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    browser = None
     language = request.config.getoption("language")
     if browser_name == "chrome":
         options = webdriver.ChromeOptions()
         options.add_experimental_option('prefs', {'intl.accept_languages': language})
         browser = webdriver.Chrome(options=options)
+        options = webdriver.ChromeOptions()
+        options.binary_location = '/usr/bin/chromium-browser'
+        # All the arguments added for chromium to work on selenium
+        options.add_argument("--no-sandbox")  # This make Chromium reachable
+        options.add_argument("--no-default-browser-check")  # Overrides default choices
+        options.add_argument("--no-first-run")
+        options.add_argument("--disable-default-apps")
+        driver = webdriver.Chrome('/home/travis/virtualenv/python2.7.9   /chromedriver', chrome_options=options)
     elif browser_name == "firefox":
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", language)
