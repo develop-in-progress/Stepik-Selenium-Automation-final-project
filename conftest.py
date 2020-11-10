27a0e58ab60556959dfba0a21b68ce2384586863
develop-in-progress <68017000+develop-in-progress@users.noreply.github.com>
2020-11-10 11:11:28 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -22,11 +22,11 @@ def browser(request):
         options = webdriver.ChromeOptions()
         options.binary_location = '/usr/bin/chromium-browser'
         # All the arguments added for chromium to work on selenium
-        options.add_argument("--no-sandbox")  # This make Chromium reachable
-        options.add_argument("--no-default-browser-check")  # Overrides default choices
-        options.add_argument("--no-first-run")
-        options.add_argument("--disable-default-apps")
-        driver = webdriver.Chrome('/home/travis/virtualenv/python2.7.9   /chromedriver', chrome_options=options)
+#         options.add_argument("--no-sandbox")  # This make Chromium reachable
+#         options.add_argument("--no-default-browser-check")  # Overrides default choices
+#         options.add_argument("--no-first-run")
+#         options.add_argument("--disable-default-apps")
+        driver = webdriver.Chrome(chrome_options=options)
     elif browser_name == "firefox":
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", language)
