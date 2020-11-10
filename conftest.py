c7d849194bfa1849b9bfd19cc54b2f608d9729bf
develop-in-progress <hellfree22@gmail.com>
2020-11-10 13:06:31 +0200

    added Travis CI

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
-
+from selenium.webdriver.firefox.options import Options as FFOptions
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
@@ -18,23 +18,23 @@ def browser(request):
     browser_name = request.config.getoption("browser_name")
     language = request.config.getoption("language")
     if browser_name == "chrome":
-        # options = webdriver.ChromeOptions()
-        # options.add_experimental_option('prefs', {'intl.accept_languages': language})
-        # browser = webdriver.Chrome(options=options)
-        # options = webdriver.ChromeOptions()
-        # options.binary_location = '/usr/bin/chromium-browser'
-        # All the arguments added for chromium to work on selenium
-#         options.add_argument("--no-sandbox")  # This make Chromium reachable
-#         options.add_argument("--no-default-browser-check")  # Overrides default choices
-#         options.add_argument("--no-first-run")
-#         options.add_argument("--disable-default-apps")
-#         driver = webdriver.Chrome(chrome_options=options)
-        browser = webdriver.Chrome(ChromeDriverManager().install())
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        options = webdriver.ChromeOptions()
+        options.add_argument("--no-sandbox")  # This make Chromium reachable
+        options.add_argument("--no-default-browser-check")  # Overrides default choices
+        options.add_argument("--no-first-run")
+        options.add_argument("--disable-default-apps")
+        options.add_argument("--headless")
+        options.add_argument('--no-sandbox')
+        browser = webdriver.Chrome(ChromeDriverManager().install(),options=options)
     elif browser_name == "firefox":
         # fp = webdriver.FirefoxProfile()
         # fp.set_preference("intl.accept_languages", language)
         # browser = webdriver.Firefox(firefox_profile=fp)
-        browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        options = FFOptions()
+        options.headless = True
+        browser = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
